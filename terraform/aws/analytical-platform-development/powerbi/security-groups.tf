f4c69d04c38eead985c049d536306beca0908492
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-10-24 16:03:50 +0000

    :wrench: Update configuration (hard coded :/ )

---
+++ terraform/aws/analytical-platform-development/powerbi/security-groups.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "powerbi" {
+  name        = "powerbi"
+  description = "allow powerbi to connect to the internet + sessions manager"
+  vpc_id      = "vpc-085ebd0bdd98a986c"
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.124.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- terraform/aws/analytical-platform-development/powerbi/windows.tf
+++ terraform/aws/analytical-platform-development/powerbi/windows.tf
@@ -4,12 +4,19 @@ module "ec2_instance" {
   name = "powerbi-testing"
 
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  key_name               = "powerbi"
   monitoring             = true
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = join(",", module.vpc.public_subnets)
+  subnet_id              = "subnet-01e566dfa9e33e4c3"
+  vpc_security_group_ids = [aws_security_group.powerbi.id]
 
-  ami  = "ami-0d8e82a3d7fda95e0" # Windows Server 2022 Base
+  ami = "ami-0d8e82a3d7fda95e0" # Windows Server 2022 Base
+
+  create_iam_instance_profile = true
+  iam_role_description        = "IAM role for EC2 instance"
+  iam_role_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
   tags = var.tags
 }
 
