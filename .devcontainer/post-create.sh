b0e4d0b180c40b6f45e0eed875a17b11f7c75ca5
Gary Henderson <26419401+Gary-H9@users.noreply.github.com>
2026-04-29 15:49:46 +0100

    feat(development container): enable `data-platform-*` EKS cluster auth (#206)
    * feat(development container): enable data-platform EKS cluster auth
    
    * velero not required for now
    
    * add kubeconfig to cspell
    
    * linting
    
    * linting
    
    * linting

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,3 +4,8 @@ set -euo pipefail
 
 # Install pre-commit hooks
 uvx pre-commit install
+
+# Install kubeconfig and AWS SSO EKS auth helper
+install --mode=0600 .devcontainer/src/kubernetes/config /home/vscode/.kube/config
+
+sudo install --mode=0770 --owner=vscode --group=vscode .devcontainer/src/kubernetes/aws-sso-eks-auth.sh /usr/local/bin/aws-sso-eks-auth
