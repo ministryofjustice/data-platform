73fcd538d8a7ae3a43e7dbc5516aed94ebc23b1f
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-04-29 14:18:06 +0000

    linting

--- .devcontainer/src/kubernetes/aws-sso-eks-auth.sh
+++ .devcontainer/src/kubernetes/aws-sso-eks-auth.sh
@@ -3,12 +3,12 @@
 aws-sso login
 
 if [[ -z "${AWS_SSO}" ]]; then
-  aws-sso exec --profile ${AWS_SSO_PROFILE} -- \
+  aws-sso exec --profile "${AWS_SSO_PROFILE}" -- \
     aws eks get-token \
-    --region ${AWS_REGION} \
-    --cluster-name ${AWS_EKS_CLUSTER_NAME}
+      --region "${AWS_REGION}" \
+      --cluster-name "${AWS_EKS_CLUSTER_NAME}"
 else
   aws eks get-token \
-    --region ${AWS_REGION} \
-    --cluster-name ${AWS_EKS_CLUSTER_NAME}
+    --region "${AWS_REGION}" \
+    --cluster-name "${AWS_EKS_CLUSTER_NAME}"
 fi
